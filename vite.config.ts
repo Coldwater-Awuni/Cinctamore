34e87059a9dafbe906dfd1e220d9bc093c7b1748
Coldwater-Awuni <danielcoldwater53@gmail.com>
2025-05-15 17:23:19 +0000

    Update vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Cinctamore/",
-  plugins: [
-    
-    react(),
-    tailwindcss(),
-  ],
+  base: '/Cinctamore/',
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
